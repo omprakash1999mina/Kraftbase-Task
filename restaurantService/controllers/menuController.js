56140e0cacd7b868e177eb81348f8ce0d5a6fb57
OP_Developer <op1999mina@gmail.com>
2024-03-11 15:15:30 +0530

    Menu Update fixed

--- restaurantService/controllers/menuController.js
+++ restaurantService/controllers/menuController.js
@@ -37,43 +37,35 @@ const menuController = {
     async update(req, res, next) {
         // console.log(req.user);
         const { error } = MenuValidation.validate(req.body);
-        if (error) return next(error);
-
+        if (error) return next(CustomErrorHandler.badRequest());
+        const restaurantId = req.user._id;
+        let document;
         try {
-            const { restaurantId, name, items, phone, address } = req.body;
-            // items = [{SN: 1, count: 2}]
-            const { _id } = req.user;
-            const exist = await User.exists({ _id: _id });
-
-            if (exist) {
-                try {
-                    let menu = await Menu.findOne({ restaurantId });
-                    let totalGrand = 0;
-                    items.forEach(it => {
-                        totalGrand += menu[it.SN].price;
-                    });
-                    await Order.create({
-                        customerId: _id,
-                        name,
-                        items,
-                        phone,
-                        totalGrand,
-                        address
-                    });
-                } catch (err) {
-                    return next(err);
-                }
-                res.status(201).json({ msg: "Order Palaced Successfully." });
+            const { items } = req.body;
+            // items = [{name: chicken, price: 200, availability: true}]
+            try {
+                document = await Menu.findOneAndReplace({ restaurantId: restaurantId },{
+                    items
+                });
+                const rest = await Restaurant.findOneAndUpdate({_id: restaurantId},{
+                    menuId: document._id
+                });
+                if(!rest) discord.SendErrorMessageToDiscord(restaurantId,"Update Menu", "Error in update restaurant menu id");
+                // console.log(document);
+            } catch (err) {
+                return next(CustomErrorHandler.serverError(err));
             }
+            res.status(200).json({ menuId: document._id, msg: "Menu Updated Successfully." });
         } catch (err) {
-            return next(CustomErrorHandler.alreadyExist('User not logged in.'));
+            discord.SendErrorMessageToDiscord(restaurantId,"Update Menu", err);
+            return next(CustomErrorHandler.serverError());
         }
     },
     async getMenu(req, res, next) {
         //  use pagination here for big data library is mongoose pagination
         let document;
         try {
-            document = await Menu.findOne({ customerId: req.params.id }).select('-__v -updatedAt -customerId');
+            document = await Menu.findOne({ customerId: req.params.id }).select('-__v ');
         } catch (err) {
             return next(CustomErrorHandler.serverError());
         }
